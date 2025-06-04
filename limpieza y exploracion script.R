ad15115ad44ebf1dd304590010eb4f90ac54b8a9
Casares-06 <inigo.casares@alumni.mondragon.edu>
2025-06-04 10:22:05 +0200

    Commmit definitivo exploracion de datos

--- limpieza y exploracion script.R
+++ limpieza y exploracion script.R
@@ -1,4 +1,31 @@
-df<-read.csv("C:/Users/gorra/Downloads/netflix.csv")
+df<-read.csv("netflix.csv")
 
-#omitir Na
+#omitir Na para la limpieza de datos .
 
+summary_stats<-function(x) {
+  x<-na.omit(x)
+  c(
+  Max = max(x),
+  Q3 = quantile(x, 0.75),
+  Mediana = median(x),
+  Media = mean(x),
+  Q1 = quantile(x, 0.25),
+  Min = min(x),
+  NAs = sum(is.na(x)),
+  DesvStd = sd(x)
+  )
+}
+minutes_stats<-summary_stats(df$Minutes)
+seasons_stats<-summary_stats(df$Seasons)
+
+minutes_stats
+seasons_stats
+
+
+unique_type <- length(unique(na.omit(df$type)))
+unique_country <- length(unique(na.omit(df$country)))
+unique_director <- length(unique(na.omit(df$director)))
+
+cat("• Type: ", unique_type, "\n")
+cat("• Country: ", unique_country, "\n")
+cat("• Director: ", unique_director, "\n")
